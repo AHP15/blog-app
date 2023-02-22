8b8afb094f6e410491927d07170e97607e64645b
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-02-22 13:01:03 +0100

    Fix linter error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  scope :last_post_comments, ->(post_id) { where(post_id: post_id).last(5) }
+  scope :last_post_comments, ->(post_id) { where(post_id:).last(5) }
 end
