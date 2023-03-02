a056a2ccedbf2b5311d220770205ca3296e60c98
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-03-02 10:14:53 +0100

    Finilize the view pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,22 @@
 class PostsController < ApplicationController
+  layout 'application'
   def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(user_id: @user.id)
+
+    @posts = @posts.map do |post|
+      comments = post.last_post_comments(post.id)
+      { 'post' => post, 'comments' => comments }
+    end
+
     render 'index'
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comments = @post.last_post_comments(@post.id)
+    @user = User.find(@post.user_id)
+
     render 'show'
   end
 end
