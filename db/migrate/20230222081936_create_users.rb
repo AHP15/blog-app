42d48df900fcdfd44567e826df3f3c5d70370900
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-03-08 16:18:59 +0100

    Add devise authentication functionality

--- db/migrate/20230222081936_create_users.rb
+++ db/migrate/20230222081936_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :photo
+      t.string :name, default: 'username'
+      t.string :photo, default: 'https://png.pngtree.com/png-vector/20190625/ourlarge/pngtree-business-male-user-avatar-vector-png-image_1511454.jpg'
       t.text :bio
-      t.integer :posts_counter
+      t.integer :posts_counter, default: 0
 
       t.timestamps
     end
